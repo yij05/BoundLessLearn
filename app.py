eab7d9d9ba5204a4aee4ae07609d0137163b5334
Jingfen21 <151896537+Jingfen21@users.noreply.github.com>
2024-10-02 01:48:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,12 @@
-from flask import Flask, redirect, url_for, session, render_template, request, flash
-from utils.common import get_student
-from services.student.room import room_bp
+from flask import Flask, render_template
+from services.student.score_analysis import get_student_score_analysis
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # 設定 Flask 的 session 加密金鑰
-app.register_blueprint(room_bp, url_prefix='/room')
 
-# 登入頁面
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        stuno = request.form['stuno']
-        password = request.form['password']
-        student = get_student(stuno, password)
-        
-        if student:
-            session['stuno'] = student['stuno']
-            flash(f"歡迎, {student['stuname']}!")
-            return redirect(url_for('room.list_rooms'))
-        else:
-            flash("無效的學號或密碼，請重試！")
-    
-    return render_template('login.html')
-
-# 登出
-@app.route('/logout')
-def logout():
-    session.clear()
-    flash("您已成功登出")
-    return redirect(url_for('login'))
+@app.route('/score_analysis/<stuno>')
+def score_analysis(stuno):
+    analysis = get_student_score_analysis(stuno)
+    return render_template('analysis_result.html', analysis=analysis)
 
 if __name__ == '__main__':
     app.run(debug=True)
